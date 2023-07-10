b19c8cc7648ef0a150afc406ca0bb780d8dafc54
Ayush Poddar <ayush.mail.id@gmail.com>
2023-07-10 15:40:16 +0530

    import MotionPathPlugin in gsap

--- assets/js/load-pixi-gsap.js
+++ assets/js/load-pixi-gsap.js
@@ -1,5 +1,8 @@
 import * as PIXI from "pixi";
 import { gsap } from "gsap";
+import { MotionPathPlugin } from "gsapMotionPathPlugin";
 
 window.PIXI = PIXI;
 window.gsap = gsap;
+
+gsap.registerPlugin(MotionPathPlugin);
