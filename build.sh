2863b49fe01fe1ce091249ef8fe9a8c28c0dc6fb
Ayush Poddar <ayush.mail.id@gmail.com>
2023-07-31 01:53:54 +0530

    build future posts in dev and staging envs

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ if [ "$CF_PAGES_BRANCH" == "main" ]; then
   hugo -e production
 else
   echo "Deploying to preview"
-  hugo -b $CF_PAGES_URL -e staging -D
+  hugo -b $CF_PAGES_URL -e staging -D --buildFuture
 fi
